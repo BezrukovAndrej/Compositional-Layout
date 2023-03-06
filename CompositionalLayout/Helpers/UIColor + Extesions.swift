51ff84181e44e6486824e834a4c81be07c889a31
Bezrukovandrej <bezrukovandrej@gmail.com>
2023-03-06 22:55:43 +0300

    Added: delegate dataSource title

---
+++ CompositionalLayout/Helpers/UIColor + Extesions.swift
@@ -0,0 +1,7 @@
+import UIKit
+
+extension UIColor {
+    static let ypBlack = UIColor(named: "YPBlack") ?? .clear
+    static let ypGray = UIColor(named: "YPGray") ?? .clear
+    static let ypWhite = UIColor(named: "YPWhite") ?? .clear
+}
--- CompositionalLayout/ViewController.swift
+++ CompositionalLayout/ViewController.swift
@@ -1,19 +1,48 @@
-//
-//  ViewController.swift
-//  CompositionalLayout
-//
-//  Created by Andrey Bezrukov on 06.03.2023.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
+    private let collectionView: UICollectionView = {
+        let collectionViewLayout = UICollectionViewLayout()
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: collectionViewLayout)
+        collectionView.backgroundColor = .ypWhite
+        collectionView.bounces = true
+        
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        return collectionView
+        
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
+    // MARK: - Private Func
+    private func setupView() {
+        view.backgroundColor = .ypWhite
+        title = "Интернет маназин"
+    }
+    private func setDelegates() {
+        collectionView.delegate = self
+        collectionView.dataSource = self
+    }
+}
+// MARK: - Properties
+private let sections = MockData.shared.pageData
 
-
+// MARK: - UICollectionViewDelegate
+extension UIViewController: UICollectionViewDelegate {
+    
+}
+// MARK: - UICollectionViewDataSource
+extension UIViewController: UICollectionViewDataSource {
+    public func numberOfSections(in collectionView: UICollectionView) -> Int {
+        sections.count
+    }
+    
+    public func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        sections[section].count
+    }
+    
+    public func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    }
 }
 
