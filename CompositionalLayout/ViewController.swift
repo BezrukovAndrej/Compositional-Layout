d350d2a5cca13170a01224f68a07c7a203d35061
Bezrukovandrej <bezrukovandrej@gmail.com>
2023-03-06 19:54:54 +0300

    Initial Commit

---
+++ CompositionalLayout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CompositionalLayout
+//
+//  Created by Andrey Bezrukov on 06.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
